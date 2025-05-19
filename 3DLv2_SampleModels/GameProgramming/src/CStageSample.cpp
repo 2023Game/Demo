07afa98adab99939f27075cfcea204edaca7b84e
Takahashi <shinobu-takahashi@anabuki-net.ne.jp>
2025-05-19 18:26:24 +0900

    Add New Model

--- 3DLv2_SampleModels/GameProgramming/src/CStageSample.cpp
+++ 3DLv2_SampleModels/GameProgramming/src/CStageSample.cpp
@@ -21,6 +21,12 @@ bool CStageSample::msLoad = true;
 #define MONSTER_PATH "res\\\monster\\monster-animated-character-X.X"
 #define MONSTER_BASE "monster\\"
 
+#define SLIME_PATH "res\\Character\\Slime\\SlimeBlue.x"
+#define SLIME_BASE "Character\\Slime\\"
+
+#define VANGU_PATH "res\\Character\\Vanguard\\VanguardModel.x"
+#define VANGU_BASE "Character\\Vanguard\\"
+
 //C:\Users\Takahashi\Desktop\Git\2023\Demo\3DLv2_SampleModels\GameProgramming\res\Character\Gas mask soldier
 
 CStageSample::CStageSample()
@@ -44,5 +50,7 @@ CStageSample::CStageSample()
 	new CModelSample(DRAGON_BASE, DRAGON_PATH, CVector(10.0f, 1.0f, 5.0f), CVector(0.0f, 180.0f, 0.0f), CVector(1.0f, 1.0f, 1.0f));
 	new CModelSample(GASMASK_BASE, GASMASK_PATH, CVector(-10.0f, -2.0f, 5.0f), CVector(0.0f, 180.0f, 0.0f), CVector(0.1f, 0.1f, 0.1f));
 	new CModelSample(MONSTER_BASE, MONSTER_PATH, CVector(-10.0f, -2.0f, 0.0f), CVector(0.0f, 180.0f, 0.0f), CVector(0.1f, 0.1f, 0.1f));
+	new CModelSample(VANGU_BASE, VANGU_PATH, CVector(0.0f, -2.0f, 0.0f), CVector(0.0f, 180.0f, 0.0f), CVector(0.1f, 0.1f, 0.1f));
+	new CModelSample(SLIME_BASE, SLIME_PATH, CVector(10.0f, -2.0f, 0.0f), CVector(0.0f, 180.0f, 0.0f), CVector(1.0f, 1.0f, 1.0f));
 
 }
